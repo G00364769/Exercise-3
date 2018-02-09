bd338db780950879ecf14c90627ad772db1726bd
G00364769 <36301378+G00364769@users.noreply.github.com>
2018-02-09 13:55:15 +0000

    Create task.py

---
+++ task.py
@@ -0,0 +1,11 @@
+#adarsha sachan
+i=int(input("Please enter the value"))
+while i!=1:
+  #print(i)
+  if (i % 2)==0:
+      i = int(i / 2)
+      #print(i)
+  else:
+      i=(i*3) +1
+      #print(i)
+  print(i)
